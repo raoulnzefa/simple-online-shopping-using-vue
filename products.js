c2e46f70e94395ecb155523f0aecbc7ead3aae01
dalia.tarek.96@hotmail.com <dalia.tarek.96@hotmail.com>
2020-06-21 12:26:43 +0200

    simple shopping

---
+++ products.js
@@ -0,0 +1,131 @@
+products=[
+    {
+        id:100,
+        name:'accessoris One',
+        price:1000,
+        instock:5,
+        description:"product One is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://placekitten.com/250/250',
+        category:'accessoris'
+    },
+    {
+        id:200,
+        name:'bages Two',
+        price:2000,
+        instock:15,
+        description:"product Two is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://www.placecage.com/250/250',
+        category:'bages'
+    },
+    {
+        id:300,
+        name:'makeup Three',
+        price:3000,
+        instock:500,
+        description:"product Three is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://picsum.photos/250/250',
+        category:'makeup'
+    },
+    {
+        id:400,
+        name:'accessoris Four',
+        price:4000,
+        instock:800,
+        description:"product Four is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250',
+        category:'accessoris'
+    },
+    {
+        id:500,
+        name:'bages Five',
+        price:5000,
+        instock:0,
+        description:"product Five is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+        ,category:'bages'
+    },
+    {
+        id:600,
+        name:'makeup Six',
+        price:6000,
+        instock:3,
+        description:"product Six is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250',
+        category:'makeup'
+    }
+    ,
+    {
+        id:30,
+        name:'accessoris Three',
+        price:3000,
+        instock:500,
+        description:"product Three is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://picsum.photos/250/250',
+        category:'accessoris'
+    },
+    {
+        id:40,
+        name:'bages Four',
+        price:4000,
+        instock:800,
+        description:"product Four is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250',
+        category:'bages'
+    },
+    {
+        id:50,
+        name:'makeup Five',
+        price:5000,
+        instock:0,
+        description:"product Five is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+        ,category:'makeup'
+    },
+    {
+        id:60,
+        name:'bages Six',
+        price:6000,
+        instock:3,
+        description:"product Six is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250',
+        category:'bages'
+    }, {
+        id:30,
+        name:'makeup Three',
+        price:3000,
+        instock:500,
+        description:"product Three is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://picsum.photos/250/250',
+        category:'makeup'
+    },
+    {
+        id:40,
+        name:'accessoris Four',
+        price:4000,
+        instock:800,
+        description:"product Four is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250',
+        category:'accessoris'
+    },
+    {
+        id:50,
+        name:'bages Five',
+        price:5000,
+        instock:0,
+        description:"product Five is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+        ,category:'bages'
+    },
+    {
+        id:60,
+        name:'makeup Six',
+        price:6000,
+        instock:3,
+        description:"product Six is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250',
+        category:'makeup'
+    }
+
+
+
+];
